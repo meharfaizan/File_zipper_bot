9ec4747e7e5f39c234b2839e402d59a02f2afef6
Chayan <150181325+captainrex32@users.noreply.github.com>
2024-03-14 11:45:13 +0530

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,14 @@
+import zipfile
+import os
+
+# ... Cloud storage imports if necessary ...
+
+def get_files_from_user(update):
+    #  Implement Telegram file receiving logic here
+    #  Return a list of file paths or file data
+
+def create_zip(file_paths):
+    # ... (Your zipping logic)
+
+def upload_to_cloud(filename):
+    # ... (Your cloud upload logic) 
